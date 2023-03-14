0e7bcfabe933bb098855ee384dee9c8b62258af8
Asmita Bhinge <aasmitabhinge@gmail.com>
2023-03-15 00:45:57 +0530

    changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 require("./db/conn");
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = 8020;
+const PORT = process.env.PORT || 8020;
 
 app.use(cors());
 app.use(express.json());
